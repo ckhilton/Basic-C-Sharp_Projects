824f09457db3f63691894e0bc5bdcffd2103275c
ckhilton <ckirkhilton@gmail.com>
2020-09-14 21:02:46 -0600

    Version 2

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -10,32 +10,30 @@ namespace myConsoleProject.cs
             Console.WriteLine("The Tech Academy\nStudent Daily Report:\n");
 
             Console.WriteLine("What is your name?");
-            string myName = "Kirk";
-            Console.WriteLine(myName + "\n");
+            string myName = Console.ReadLine();
 
-            Console.WriteLine("What course are you on?");
-            string courseName = "C# & .NET Framework Course\n";
-            Console.WriteLine(courseName);
+            Console.WriteLine("\nWhat course are you on?");
+            string courseName = Console.ReadLine();
+            
+            Console.WriteLine("\nWhat page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
 
-            Console.WriteLine("What page number?");
-            ushort pageNumber = 133;
-            Console.WriteLine(pageNumber + "\n");
+            Console.WriteLine("\nDo you need help with anything? Please answer \"true\" or \"false\"");
+            string helpNeeded = Console.ReadLine();
+            bool helpNeed = Convert.ToBoolean(helpNeeded);
 
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
-            bool helpNeeded = false;
-            Console.WriteLine(helpNeeded + "\n");
+            Console.WriteLine("\nWere there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
 
-            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-            string positiveExperiences = "Just moving forward and more learning C#.";
-            Console.WriteLine(positiveExperiences + "\n");
+            Console.WriteLine("\nIs there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
 
-            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
-            string feedback = "I'm not certain if this is the format you wanted this assignment in.";
-            Console.WriteLine(feedback + "\n");
+            Console.WriteLine("\nHow many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            int hoursStud = Convert.ToInt32(hoursStudied);
 
-            Console.WriteLine("How many hours did you study today?");
-            ushort hoursStudied = 8;
-            Console.WriteLine(hoursStudied);
+            Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
 
             Console.ReadLine();
 
