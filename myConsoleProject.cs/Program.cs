4a74c2ab0617e9e1f81a3b3bdb067edf55d39ff4
ckhilton <ckirkhilton@gmail.com>
2020-09-14 20:02:46 -0600

    Version 1

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -6,40 +6,39 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
-            //Console.WriteLine("What is your name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("Hello, my name is: \n" + name + "!");
-            //Console.Read();
 
-            //Console.WriteLine("What is your favorite number?");
-            //string favoriteNumber = Console.ReadLine();
-            //int favNum = Convert.ToInt32(favoriteNumber);
-            //int total = favNum + 5;
-            //Console.WriteLine("Your favorite number plus 5 is: " + total);
-            //Console.ReadLine();
+            Console.WriteLine("The Tech Academy\nStudent Daily Report:\n");
 
+            Console.WriteLine("What is your name?");
+            string myName = "Kirk";
+            Console.WriteLine(myName + "\n");
 
-            //THE FOLLOWING ARE JUST EXAMPLES OF DATA TYPES
-            bool isStudying = false;
-            byte hoursWorked = 42;
-            sbyte currentTemperature = -23;
-            char oneCharacter = 't';
-            char randomLetter = 'X';
-            char questionMark = '\u2103';
+            Console.WriteLine("What course are you on?");
+            string courseName = "C# & .NET Framework Course\n";
+            Console.WriteLine(courseName);
 
-            decimal moneyInBank = 100.5m; //NOTE THE 'm' IS REQUIRED AFTER DECIMALS (WAS MADE FOR MONEY)
-            double heightInCentimeters = 211.30204234; //ANY NUMBER 15 OR 16 UNITS IN LENGTH (CAN HAVE DECIMALS IN IT)
-            float secondsLeft = 2.62f; //ONLY UP TO 7 DIGITS IN LENGTH. NOTE THE 'f' IS NEEDED TO NO IT'S A FLOAT AND NOT A DECIMAL. 
-            short tempepratureOnMars = -341; //WHOLE NUMBER BETWEEN -32000 AND 32000
-            string myName = "Kirk"; //NEEDS DOUBLE QUOTES
+            Console.WriteLine("What page number?");
+            ushort pageNumber = 133;
+            Console.WriteLine(pageNumber + "\n");
 
-            int currentAge = 30;
-            string yearsOld = currentAge.ToString();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            bool helpNeeded = false;
+            Console.WriteLine(helpNeeded + "\n");
 
-            bool isRaining = true;
-            string rainingStatus = Convert.ToString(isRaining);
-            Console.WriteLine(isRaining);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = "Just moving forward and more learning C#.";
+            Console.WriteLine(positiveExperiences + "\n");
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = "I'm not certain if this is the format you wanted this assignment in.";
+            Console.WriteLine(feedback + "\n");
+
+            Console.WriteLine("How many hours did you study today?");
+            ushort hoursStudied = 8;
+            Console.WriteLine(hoursStudied);
+
             Console.ReadLine();
+
         }
     }
 }
