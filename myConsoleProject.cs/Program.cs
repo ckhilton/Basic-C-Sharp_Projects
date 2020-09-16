d86aa601893ac9265348f629a55c332fd5b5be3f
ckhilton <ckirkhilton@gmail.com>
2020-09-15 22:08:45 -0600

    Step 161 Assignment

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -6,10 +6,25 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, my name is: \n" + name + "!");
-            Console.Read();
+
+            Console.Write("Welcome to the Insurance Qualifier app.\nLet's see if you qualify for car insurance today.");
+
+            Console.WriteLine("\n\nWhat is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nHave you ever had a DUI? (true or false)");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nQualified?");
+            //==== BOOLEAN LOGIC FOR BUSINESS RULES
+            bool qualified = (age > 15) && (DUI == false) && (tickets <= 3);
+            Console.Write(qualified);
+
+            Console.ReadLine();
+
         }
     }
 }
