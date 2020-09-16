7a14dc581ada957f7a5ed8cb62c316a2aa9eddfe
ckhilton <ckirkhilton@gmail.com>
2020-09-15 21:31:48 -0600

    Step 152 Assignment

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace myConsoleProject.cs
 {
@@ -6,10 +10,33 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, my name is: \n" + name + "!");
-            Console.Read();
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("\nPerson 1:\nHourly Rate?");
+            float p1Hourly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            float p1Hours = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPerson 2:\nHourly Rate?");
+            float p2Hourly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            float p2Hours = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nAnnual salary of Person 1:");
+            //MATH FOR ANNUAL SALARY
+            float p1Salary = p1Hourly * p1Hours * 52;
+            Console.WriteLine("$" + p1Salary);
+
+            Console.WriteLine("\nAnnual salary of Person 2:");
+            //MATH FOR ANNUAL SALARY
+            float p2Salary = p2Hourly * p2Hours * 52;
+            Console.WriteLine("$" + p2Salary);
+
+            Console.WriteLine("\nDoes Person 1 make more money than Person 2?");
+            //BOOLEAN COMPARISON
+            Console.WriteLine(p1Salary > p2Salary);
+
+            Console.ReadLine();
         }
     }
 }
