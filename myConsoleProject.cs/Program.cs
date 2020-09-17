40bd3c4fd1eba06ed4d553654a9f43398632609a
ckhilton <ckirkhilton@gmail.com>
2020-09-17 15:01:23 -0600

    Iteration Video

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -10,10 +10,66 @@ namespace myConsoleProject.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, my name is: \n" + name + "!");
-            Console.ReadLine();
+            //ITERATION
+
+            ////INSTANTIATE INTEGER ARRAY WITH TEST SCORES
+            //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+
+            //ITERATION THROUGH AN ARRAY OF STRINGS
+            //string[] names = { "Kirk", "Jesse", "Erik", "Daniel", "Adam" };
+
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    Console.WriteLine(names[j]);
+            //}
+
+            //ITERATION THROUGH A LIST
+            //INSTATIATE A NEW LIST OBJECT CALLED 'testScores' USING THE CLASS 'List<int>'
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(99);
+            //testScores.Add(81);
+            //testScores.Add(72);
+            //testScores.Add(70);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + score);
+            //    }
+            //}
+
+            //List<string> names = new List<string>() { "Kirk", "Jesse", "Erik", "Adam", "Daniel" };
+
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            //List<int> passingScores = new List<int>();
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        passingScores.Add(score);
+            //    }
+            //    //WILL ITERATE THROUGH AND DISPLAY EACH PASSING SCORE ON A NEW LINE
+            //    Console.WriteLine(score);
+            //}
+
+            //Console.WriteLine("There were " + passingScores.Count + " passing scores.");
+            //Console.ReadLine();
         }
     }
 }
