fef759fc9fc54cf4124cd046275daf2cd83f5c08
ckhilton <ckirkhilton@gmail.com>
2020-09-17 10:59:16 -0600

    Update

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -7,27 +7,96 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
+            //DISPLAY STORY INFO
+            Console.WriteLine("//==== STORY/STEP 1 (INCLUDES REQUEST IN STORY/STEP 3) =====");
+
             //INSTANTIATE ONE-DIMENSIONAL ARRAY OF STRINGS
             string[] strArray = { "Happy", "Joy", "Glad", "Smile" };
             //ASK USER TO SELECT AN INDEX
-            Console.WriteLine("Please select an index value between 0 and 3:\n");
+            Console.WriteLine("\nPlease select an index value between 0 and 3:\n");
             //READ USER INPUT AND PASS INTO INTEGER VARIABLE CALLED 'strIndex'
             int strIndex = Convert.ToInt32(Console.ReadLine());
-            //DISPLAY THE STRING AT THE INDEX THE USER CHOOSES IN 'strArray'
-            Console.WriteLine("\n\tThe word you chose at index " + strIndex + " is: " + "\"" + strArray[strIndex] + "\"");
+
+            //ADD MESSAGE IF INDEX CHOICE DOESN'T EXIST. ALLOW USER TO TRY AGAIN USING SWITCH AND DO WHILE LOOP
+            bool strChoice = strIndex < 4; /*(strIndex == 1) || (strIndex == 2) || (strIndex == 3);*/
+
+            do
+            {
+                if (!strChoice)
+                {
+                    Console.WriteLine("\n\tYou chose: " + strIndex +
+                            "\n\tThat index does not exist." +
+                            "\n\tPlease select an index between 0 and 3.");
+                    strIndex = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+
+                }
+
+
+            }
+
+            do
+            {
+                switch (strIndex)
+                {
+                    case 0:
+                        //DISPLAY THE STRING AT THE INDEX THE USER CHOOSES IN 'strArray' (USE OF VARIABLES TO COPY AND PASTE CODE FASTER)
+                        Console.WriteLine("\n\tThe word you chose at index " + strIndex + " is: " + "\"" + strArray[strIndex] + "\"");
+                        strChoice = true;
+                        break;
+                    case 1:
+                        Console.WriteLine("\n\tThe word you chose at index " + strIndex + " is: " + "\"" + strArray[strIndex] + "\"");
+                        strChoice = true;
+                        break;
+                    case 2:
+                        Console.WriteLine("\n\tThe word you chose at index " + strIndex + " is: " + "\"" + strArray[strIndex] + "\"");
+                        strChoice = true;
+                        break;
+                    case 3:
+                        Console.WriteLine("\n\tThe word you chose at index " + strIndex + " is: " + "\"" + strArray[strIndex] + "\"");
+                        strChoice = true;
+                        break;
+                    default:
+                        Console.WriteLine("\n\tYou chose: " + strIndex +
+                            "\n\tThat index does not exist." +
+                            "\n\tPlease select an index between 0 and 3.");
+                        //READ USER INPUT AGAIN
+                        strIndex = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!strChoice);   
 
             //==== PROGRAM PAUSE ====
             Console.WriteLine("\nPRESS ENTER TO CONTINUE");
             Console.ReadLine();
 
+            //DISPLAY STORY(STEP) INFO
+            Console.WriteLine("//==== STORY/STEP 2 (INCLUDES REQUEST IN STORY/STEP 3) =====");
             //INSTANTIATE ONE-DIMENSIONAL ARRAY OF INTEGERS
             int[] intArray = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
             // ASK USER TO SELECT AN INDEX
-            Console.WriteLine("Please select an index value between 0 and 10:\n");
+            Console.WriteLine("\nPlease select an index value between 0 and 10:\n");
             //READ USER INPUT AND PASS INTO VARIABLE CALLED 'intIndex'
             int intIndex = Convert.ToInt32(Console.ReadLine());
-            //DISPLAY THE STRING AT THE INDEX THE USER CHOOSES IN 'intArray'
-            Console.WriteLine("\n\tThe integer you chose at index " + intIndex + " is: " + intArray[intIndex]);
+
+            //ADD MESSAGE IF INDEX CHOICE DOESN'T EXIST. ALLOW USER TO TRY AGAIN USING SWITCH AND DO WHILE LOOP
+            bool intChoice = (intIndex < 11);
+
+            do
+            {
+                switch (intIndex)
+                { 
+                    case 0:
+                        //DISPLAY THE STRING AT THE INDEX THE USER CHOOSES IN 'strArray' (USE OF VARIABLES TO COPY AND PASTE CODE FASTER)
+                        Console.WriteLine("\n\tThe integer you chose at index " + intIndex + " is: " + intArray[intIndex]);
+                    intChoice = true;
+                    break;
+                }
+            }
+            while (!intChoice);
 
             Console.ReadLine();
 
