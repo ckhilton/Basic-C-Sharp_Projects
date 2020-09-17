74a7e5df8986b804b5931f9e137320eb54ca4eaf
ckhilton <ckirkhilton@gmail.com>
2020-09-16 20:04:14 -0600

    Working with strings

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace myConsoleProject.cs
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, my name is: \n" + name + "!");
-            Console.Read();
+            //string name = "Kirk";
+            //string quote = "The man said, \"Hello\", to me.";
+            //string fileName = "C:\\Users\\Kirk"; //USE TWO '\' IN ORDER TO ESCAPE ITSELF
+            //string fileName = @"C\Users\Kirk"; //USE THE @ CHARACTER TO TELL THE STRING THAT THERE ARE NO ESCAPE SEQUENCES USED
+
+            //bool trueOrFalse = name.Contains("r"); //OUTPUT WILL BE TRUE
+            //trueOrFalse = name.EndsWith("K"); //OUTPUT WILL BE TRUE
+            //int length = name.Length; //WILL PRINT THE NUMBER OF CHARACTERS TO THE CONSOLE (NOTE: *THERE IS NO () AFTER 'Length')
+            //name = name.ToLower(); //ALL LOWERCASE
+            //name = name.ToUpper(); //ALL UPPERCASE
+
+            //Console.WriteLine(name);
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("My name is Kirk");
+
+            //Console.WriteLine(sb);
+            //Console.ReadLine();
         }
     }
 }
