2033825a9465c556aa049b0632852a1a484fd1b8
ckhilton <ckirkhilton@gmail.com>
2020-09-19 02:10:12 -0600

    Save

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -96,125 +96,254 @@ namespace myConsoleProject.cs
             //Console.ReadLine();
 
             //============= CONSOLE APP ASSSIGNMENT STEP 200 ====================
-            Console.WriteLine("//===== CONSOLE APP ASSIGNMENT STEP 200 =====");
-            //GIVE INSTRUCTIONS
-            Console.WriteLine("\nPRESS ENTER TO VIEW THE CONTESTANT LIST OF THE 2020 HILTON FAMILY ANNUAL THUMB-WAR BATTLE");
+            //Console.WriteLine("//===== CONSOLE APP ASSIGNMENT STEP 200 =====");
+            ////GIVE INSTRUCTIONS
+            //Console.WriteLine("\nPRESS ENTER TO VIEW THE CONTESTANT LIST OF THE 2020 HILTON FAMILY ANNUAL THUMB-WAR TOURNAMENT");
+            //Console.ReadLine();
+            //Console.WriteLine("\tCONTESTANTS:" +
+            //    "\n\n\tVince\n\tDrew" +
+            //    "\n\tKirk\n\tSteph" +
+            //    "\n\tRob\n\tDave" +
+            //    "\n\tJeff\n\tBrighton" +
+            //    "\n\tLily\n\tMaisy");
+            ////ASK USER TO SEARCH FOR A FINALIST NAME IN THE LIST
+            //Console.WriteLine("\nNOW TYPE ANY NAME FROM ABOVE " +
+            //    "AND SEE WHICH PLACE (INDEX) EACH CONTESTANT TOOK IN THE TOURNAMENT.\n");
+            ////INSTANTIATE LIST OF ACTUAL FINALISTS
+            //List<string> contestant_List = new List<string>() { "referee", "lily", "brighton", "steph", "vince", "drew", "rob", "dave", "jeff", "maisy", "kirk" };
+
+            ////SETUP VARIABLE TO CONTROL DO-WHILE LOOP
+            //bool search = true;
+
+            //do
+            //{
+            //    //STORE USER INPUT AND STORE AS VARIABLE 'inputChoice'
+            //    string inputChoice = Console.ReadLine();
+            //    //CONTROL USER INPUT
+            //    string inputLower = inputChoice.ToLower();
+
+            //    //ITERATE THROUGH LIST AND SEARCH FOR MATCHING CONTESTANTS
+            //    if (contestant_List.Contains(inputLower))
+            //    {
+            //        //GIVE USER THE NAME AND PLACE OF THE CONTESTANT IN THE TOURNAMENT (PLACE = INDEX NUMBER).
+            //        Console.WriteLine("\n\t" + inputLower.ToUpper() + " FINISHED IN THE NUMBER " + contestant_List.IndexOf(inputLower) + " SPOT." +
+            //            "\n\n\tENTER ANOTHER CONTESTANT NAME OR PRESS 'X' TO MOVE ON\n");
+            //    }
+            //    //OPTION TO EXIT LOOP IF DESIRED
+            //    else if (inputChoice == "x")
+            //    {
+            //        //STOP LOOP
+            //        search = false;
+            //    }
+            //    else
+            //    {
+            //        //MESSAGE TELLING USER THAT TEXT IS NOT VALID
+            //        Console.WriteLine("\nNOT A VALID ENTRY FROM THE LIST. PLEASE CHECK THE SPELLING AND TRY ANOTHER NAME.\n");
+            //    }               
+            //}
+            //while (search);
+
+            //============= CONSOLE APP ASSSIGNMENT STEP 201 ====================
+            Console.WriteLine("//===== CONSOLE APP ASSIGNMENT STEP 201 =====");
+            Console.WriteLine("\nPRESS ENTER TO VIEW A LIST OF PEOPLE I CLIMB WITH");
             Console.ReadLine();
-            Console.WriteLine("\tCONTESTANTS:" +
-                "\n\n\tVince\n\tDrew" +
-                "\n\tKirk\n\tSteph" +
-                "\n\tRob\n\tDave" +
-                "\n\tJeff\n\tBrighton" +
-                "\n\tLily\n\tMaisy");
+            Console.WriteLine("\tCLIMBING PARTNERS:" +
+                "\n\n\tMike\n\tBrad" +
+                "\n\tScott\n\tNathan" +
+                "\n\tJohn\n\tWes" +
+                "\n\tJason\n\tZac");
             //ASK USER TO SEARCH FOR A FINALIST NAME IN THE LIST
             Console.WriteLine("\nNOW TYPE ANY NAME FROM ABOVE " +
-                "TO SEE IF THEY MADE IT TO THE FINALS ROUND\n");
+                "AND SEE HOW MANY TIMES AGO I CLIMBED WITH EACH PARTNER.\n");
             //INSTANTIATE LIST OF ACTUAL FINALISTS
-            List<string> finalist_List = new List<string>() { "Vince", "Drew", "Kirk", "Steph", "Rob", "Dave", "Jeff", "Brighton", "Lily", "Maisy" };
-            //STORE USER INPUT AND STORE AS VARIABLE 'contestant'
-            string contestant = Console.ReadLine();
+            //List<string> partnerList = new List<string>() { "NONE", "scott", "mike", "jason", "scott", "brad", "nathan", "wes", "nathan", "wes", "jason", "brad", "mike" };
+            string[] partnerArray = { "NONE", "scott", "mike", "jason", "scott", "brad", "nathan", "wes", "nathan", "wes", "jason", "brad", "mike" };
 
-            
-            //SET UP VARIABLE FOR DO-WHILE LOOP
-            bool contest = false;
+            ////store user input and store as variable 'inputpartner'
+            string inputPartner = Console.ReadLine();
+            ////CONTROL USER INPUT
+            //string partnerLower = inputPartner.ToLower();
 
-            do
+            int number = Array.IndexOf(partnerArray, inputPartner);
+
+            foreach (string partner in partnerArray)
             {
-                switch (contestant)
+                if (partner == inputPartner)
                 {
-                    case string 'Kirk':
 
-
-
+                    Console.WriteLine(number);
                 }
             }
-            while (!contest);
+
+
+
+
+
+
+            //bool find = true;
+
+            //do
+            //{
+            //    //STORE USER INPUT AND STORE AS VARIABLE 'inputPartner'
+            //    string inputPartner = Console.ReadLine();
+            //    //CONTROL USER INPUT
+            //    string partnerLower = inputPartner.ToLower();
+
+            //    //BRANCHING CONDITIONS TO BE MET
+            //    //if (partnerArray.Contains(partnerLower))
+            //    //{
+            //    //string[] partnerArray = partnerList.ToArray();
+
+            //    //int i = 0;
+
+            //    //int indexOfChoice = Convert.ToInt32(partnerList.IndexOf(partnerLower));
+
+            //    //int[] index = new int[partnerArray.Length];
+
+            //    //for (int partner in partnerArray)
+            //    //{
+            //    for (int i = 0; i < partnerArray.Count(); i++)
+            //    {
+            //        foreach (int partner in partnerArray[i])
+            //        {
+            //            if ()
+                        
+
+            //            }
+                        
+
+            //            //GIVE USER THE NAME AND PLACE OF THE CONTESTANT IN THE TOURNAMENT (PLACE = INDEX NUMBER)
+            //            Console.WriteLine("\n\t" + partnerLower.ToUpper() + " WENT CLIMBING WITH ME THESE TIMES: " + partnerArray[i] +
+
+
+            //            "\n\n\tENTER ANOTHER PARTNER NAME OR PRESS 'X' TO MOVE ON ");
+            //        }
+
+            //    }
+
+                
+                   
+                    
+                    
+                
+
+            //    //}
+            //    //OPTION TO EXIT LOOP IF DESIRED
+            //    //else if (inputPartner == "x")
+            //    //{
+            //    //    //STOP LOOP
+            //    //    find = false;
+            //    //}
+            //    //else
+            //    //{
+            //    //    Console.WriteLine("\nNOT A VALID ENTRY FROM THE LIST. PLEASE CHECK THE SPELLING AND TRY ANOTHER NAME.\n");
+            //    //}
+                
+            //}
+            //while (find);
 
             Console.ReadLine();
-
-            //foreach (string finalist in finalist_List)
-            //{
-            //    if (finalist != contestant)
-            //    {
-            //        Console.WriteLine("\n" + contestant + " lost and did not make it to the finals. Try another name.\n");
-            //        contestant = Console.ReadLine();
-            //        //contest = true;
-            //    }
-            //    if (finalist == contestant)
-            //    {
-            //        Console.WriteLine("\nYep! " + finalist + " made it to the finals. Try another name.\n");
-            //        contestant = Console.ReadLine();            
-            //    }
-            //}   
-
-
-
-
-
-
-
-
-
-            //ITERATION
-
-            ////INSTANTIATE INTEGER ARRAY WITH TEST SCORES
-            //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
-
-            //for (int i = 0; i < testScores.Length; i++)
-            //{
-            //    if (testScores[i] > 85)
-            //    {
-            //        Console.WriteLine("Passing test score: " + testScores[i]);
-            //    }
-            //}
-
-            //ITERATION THROUGH AN ARRAY OF STRINGS
-            //string[] names = { "Kirk", "Jesse", "Erik", "Daniel", "Adam" };
-
-            //for (int j = 0; j < names.Length; j++)
-            //{
-            //    Console.WriteLine(names[j]);
-            //}
-
-            //ITERATION THROUGH A LIST
-            //INSTATIATE A NEW LIST OBJECT CALLED 'testScores' USING THE CLASS 'List<int>'
-            //List<int> testScores = new List<int>();
-            //testScores.Add(98);
-            //testScores.Add(99);
-            //testScores.Add(81);
-            //testScores.Add(72);
-            //testScores.Add(70);
-
-            //foreach (int score in testScores)
-            //{
-            //    if (score > 85)
-            //    {
-            //        Console.WriteLine("Passing test score: " + score);
-            //    }
-            //}
-
-            //List<string> names = new List<string>() { "Kirk", "Jesse", "Erik", "Adam", "Daniel" };
-
-            //foreach (string name in names)
-            //{
-            //    Console.WriteLine(name);
-            //}
-
-            //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
-            //List<int> passingScores = new List<int>();
-
-            //foreach (int score in testScores)
-            //{
-            //    if (score > 85)
-            //    {
-            //        passingScores.Add(score);
-            //    }
-            //    //WILL ITERATE THROUGH AND DISPLAY EACH PASSING SCORE ON A NEW LINE
-            //    Console.WriteLine(score);
-            //}
-
-            //Console.WriteLine("There were " + passingScores.Count + " passing scores.");
-            //Console.ReadLine();
         }
     }
 }
+
+
+
+////var listClimbs = Enumerable.Range(0, partnerList.Count)
+////    .Where(i => partnerList[i] == partnerLower)
+////    .ToList();
+
+//{
+//    switch (contestant)
+//    {
+//        case string 'Kirk':
+
+
+
+//    }
+//}
+
+//foreach (string finalist in finalist_List)
+//{
+//    if (finalist != contestant)
+//    {
+//        Console.WriteLine("\n" + contestant + " lost and did not make it to the finals. Try another name.\n");
+//        contestant = Console.ReadLine();
+//        //contest = true;
+//    }
+//    if (finalist == contestant)
+//    {
+//        Console.WriteLine("\nYep! " + finalist + " made it to the finals. Try another name.\n");
+//        contestant = Console.ReadLine();            
+//    }
+//}   
+
+
+
+
+
+
+
+
+
+//ITERATION
+
+////INSTANTIATE INTEGER ARRAY WITH TEST SCORES
+//int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+//for (int i = 0; i < testScores.Length; i++)
+//{
+//    if (testScores[i] > 85)
+//    {
+//        Console.WriteLine("Passing test score: " + testScores[i]);
+//    }
+//}
+
+//ITERATION THROUGH AN ARRAY OF STRINGS
+//string[] names = { "Kirk", "Jesse", "Erik", "Daniel", "Adam" };
+
+//for (int j = 0; j < names.Length; j++)
+//{
+//    Console.WriteLine(names[j]);
+//}
+
+//ITERATION THROUGH A LIST
+//INSTATIATE A NEW LIST OBJECT CALLED 'testScores' USING THE CLASS 'List<int>'
+//List<int> testScores = new List<int>();
+//testScores.Add(98);
+//testScores.Add(99);
+//testScores.Add(81);
+//testScores.Add(72);
+//testScores.Add(70);
+
+//foreach (int score in testScores)
+//{
+//    if (score > 85)
+//    {
+//        Console.WriteLine("Passing test score: " + score);
+//    }
+//}
+
+//List<string> names = new List<string>() { "Kirk", "Jesse", "Erik", "Adam", "Daniel" };
+
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+//List<int> passingScores = new List<int>();
+
+//foreach (int score in testScores)
+//{
+//    if (score > 85)
+//    {
+//        passingScores.Add(score);
+//    }
+//    //WILL ITERATE THROUGH AND DISPLAY EACH PASSING SCORE ON A NEW LINE
+//    Console.WriteLine(score);
+//}
+
+//Console.WriteLine("There were " + passingScores.Count + " passing scores.");
+//Console.ReadLine();
+
