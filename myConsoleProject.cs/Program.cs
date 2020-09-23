ebf28b7ab1b9f4114e8acf4be18bc9654eca57a6
ckhilton <ckirkhilton@gmail.com>
2020-09-23 00:28:52 -0600

    ExceptionHandling

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -10,10 +10,35 @@ namespace myConsoleProject.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, my name is: \n" + name + "!");
-            Console.ReadLine();
-        }
+            try //WHAT WE WANT IT TO DO
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two numbers...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message); //COULD ALSO WRITE A CUSTOM MESSAGE LIKE THIS... Console.WriteLine("PLEASE TYPE A WHOLE NUMBER");
+                return; //STOPS THE PROGRAM AND MAY SENT THE USER TO ANOTHER PAGE, ETC.
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("PLEASE DON'T DIVIDE BY ZERO");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); //GENERIC EXCEPTION MESSAGE
+            }
+            finally //ALWAYS RUNS NO MATTER WHAT, USUALLY USED FOR LOGGING ERRORS TO A DATABASE
+            {
+                Console.ReadLine();
+            }
+                     
+        }           
     }
 }
