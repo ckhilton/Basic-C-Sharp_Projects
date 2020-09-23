ec897e1fce76a3ed8a5ab1da19af3f1dec33bca1
ckhilton <ckirkhilton@gmail.com>
2020-09-23 03:32:33 -0600

    Step 207 Assignment

--- myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/Program.cs
@@ -10,35 +10,82 @@ namespace myConsoleProject.cs
     {
         static void Main(string[] args)
         {
-            try //WHAT WE WANT IT TO DO
+            //========================== STEP 207 ASSIGNMENT ==============================
+            Console.Write("\n=========================== STEP 207 ASSIGNMENT =============================\n" +
+                "\n\t(PRESS ENTER TO DISPLAY A LIST OF MY CHOSEN INTEGERS)\n");
+            Console.ReadLine();
+
+            //CREATE A LIST OF INTEGERS
+            List<int> integers = new List<int>() { 100, 50, 8, 364, 24, 30, 66 };
+            //PRINT LIST TO CONSOLE
+            foreach (int integer in integers)
             {
-                Console.WriteLine("Pick a number.");
-                int numberOne = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Pick another number.");
-                int numberTwo = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Dividing the two numbers...");
-                int numberThree = numberOne / numberTwo;
-                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
-                Console.ReadLine();
+                Console.WriteLine(integer);
             }
-            catch (FormatException ex)
+            Console.WriteLine("");
+
+            //CONTROL VARIABLE
+            bool executing = true;
+
+            //ENTER DO-WHILE LOOP (FOR EASE OF TESTING PROGRAM)
+            do
             {
-                Console.WriteLine(ex.Message); //COULD ALSO WRITE A CUSTOM MESSAGE LIKE THIS... Console.WriteLine("PLEASE TYPE A WHOLE NUMBER");
-                return; //STOPS THE PROGRAM AND MAY SENT THE USER TO ANOTHER PAGE, ETC.
+                //THE CODE I WANT TO WORK
+                try
+                {                    
+                    
+                    //GET USER INPUT
+                    Console.WriteLine("TYPE IN ANY RANDOM NUMBER YOU WANT\n");
+                    //STORE USER INPUT IN VARIABLE
+                    int userNumber = Convert.ToInt32(Console.ReadLine());                                                                               
+                    //CREATE LOOP
+                    foreach (int integer in integers)
+                    {
+                        int answer = integer / userNumber;
+                        Console.WriteLine("\n\t" + integer + " \u00F7 " + userNumber + " = " + answer);
+                    }
+                }
+
+                //================ EXCEPTION HANDLING ================
+                //DIVIDE BY ZERO CATCH
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("\nNICE TRY! YOU " + ex.Message.ToUpper() + " PLEASE CHOOSE A NON-ZERO NUMBER." +
+                        "\n\nPRESS ENTER TO LEAVE THE TRY-CATCH BLOCK");
+                    Console.ReadLine();
+                    
+                }
+                //WRONG FORMAT CATCH
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("\nTISK TISK! TEXT NOT ALLOWED. NULL IS NOT ACCEPTABLE EITHER!" +
+                        "\n\nPRESS ENTER TO LEAVE THE TRY-CATCH BLOCK");
+                    Console.ReadLine();                   
+                }
+                //GENERAL EXCEPTION CATCH
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToUpper() +
+                        "\n\nPRESS ENTER TO LEAVE THE TRY-CATCH BLOCK");
+                    Console.ReadLine();
+                }
+                //INSTRUCTIONS TO USER (***NOTE TO SELF: FINALLY BLOCK ALWAYS EXECUTES NO MATTER WHAT)
+                finally
+                {
+                    //PROGRAM NOTE
+                    Console.Write("\n===== THE PROGRAM HAS NOW EMERGED FROM THE TRY/CATCH BLOCK. =====\n" +
+                        "\n\t  *** (PRESS ENTER TO TEST ANOTHER INPUT) ***\n\n\t\t\t" +
+                        "     OR\n\n" +
+                        "\t    XXX (PRESS \'X\' THEN ENTER TO EXIT) XXX\n");
+                    string exit = Console.ReadLine();
+                    //OPTION TO EXIT WITH CONTROLLED CASE SENSITIVITY
+                    if ((exit == "x") || (exit == "X"))
+                    {
+                        executing = false;                      
+                    }
+                }               
             }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("PLEASE DON'T DIVIDE BY ZERO");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message); //GENERIC EXCEPTION MESSAGE
-            }
-            finally //ALWAYS RUNS NO MATTER WHAT, USUALLY USED FOR LOGGING ERRORS TO A DATABASE
-            {
-                Console.ReadLine();
-            }
-                     
+            while (executing);
         }           
     }
 }
